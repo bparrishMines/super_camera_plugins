38c3229e6f24b6d2a6d578d2c06d527e89efe23f
Maurice Parrish <bmparr@google.com>
2019-03-18 12:54:05 -0700

    Setup super_camera library

---
+++ super_camera/lib/src/camera.dart
@@ -0,0 +1,11 @@
+part of super_camera;
+
+class Camera {
+  Camera._();
+
+  @visibleForTesting
+  static const MethodChannel channel = MethodChannel(
+      'bmparr.plugins/super_camera');
+
+  static Future<List<CameraDevice>> availableCameras();
+}
--- super_camera/lib/super_camera.dart
+++ super_camera/lib/super_camera.dart
@@ -1,13 +1,6 @@
-import 'dart:async';
+library super_camera;
 
 import 'package:flutter/services.dart';
+import 'package:meta/meta.dart';
 
-class SuperCamera {
-  static const MethodChannel _channel =
-      const MethodChannel('super_camera');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+part 'src/camera.dart';
