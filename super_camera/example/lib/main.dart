09fc2f16ced78796528eff43d6f29da118050f55
Maurice Parrish <bmparr@google.com>
2019-03-18 17:34:41 -0700

    Get camera permission

--- super_camera/example/lib/main.dart
+++ super_camera/example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:super_camera/super_camera.dart';
 
 void main() => runApp(MyApp());
@@ -16,8 +17,25 @@ class _MyAppState extends State<MyApp> {
   }
 
   Future<void> testCameraPlugin() async {
+    final bool hasCameraAccess = await getCameraPermission();
     final List<CameraDevice> cameras = await Camera.availableCameras();
     print(cameras);
+  }
+
+  Future<bool> getCameraPermission() async {
+    final PermissionStatus permission =
+        await PermissionHandler().checkPermissionStatus(
+      PermissionGroup.camera,
+    );
+
+    if (permission == PermissionStatus.granted) {
+      return true;
+    }
+
+    final Map<PermissionGroup, PermissionStatus> permissions =
+        await PermissionHandler().requestPermissions([PermissionGroup.camera]);
+
+    return permissions[PermissionGroup.camera] == PermissionStatus.granted;
   }
 
   @override
