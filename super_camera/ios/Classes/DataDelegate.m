135a1db287e49a3a23e3b61fce6a0796260081c4
Maurice Parrish <bmparr@google.com>
2019-03-27 21:06:40 -0700

    Fix bug on iOS

--- super_camera/ios/Classes/DataDelegate.m
+++ super_camera/ios/Classes/DataDelegate.m
@@ -16,25 +16,7 @@
 }
 
 - (void)onImageTaken:(CMSampleBufferRef _Nullable)imageDataSampleBuffer error:(NSError *_Nullable)error {
-  if (error) {
-    NSLog(error.description);
-  }
-  CVPixelBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(imageDataSampleBuffer);
-  _result([self toData:pixelBuffer]);
-}
-
-- (NSData *)toData:(CVPixelBufferRef)pixelBuffer {
-  CVPixelBufferLockBaseAddress(pixelBuffer, kCVPixelBufferLock_ReadOnly);
-
-  void *planeAddress = CVPixelBufferGetBaseAddress(pixelBuffer);
-  size_t bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer);
-  size_t height = CVPixelBufferGetHeight(pixelBuffer);
-
-  NSNumber *length = @(bytesPerRow * height);
-  NSData *bytes = [NSData dataWithBytes:planeAddress length:length.unsignedIntegerValue];
-
-  CVPixelBufferUnlockBaseAddress(pixelBuffer, kCVPixelBufferLock_ReadOnly);
-
-  return bytes;
+  NSData *bytes = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
+  _result(bytes);
 }
 @end
