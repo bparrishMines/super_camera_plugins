26f50b5b7b805973748efa88628935ebb79e9646
Maurice Parrish <bmparr@google.com>
2019-03-28 10:29:30 -0700

    Add camera exception types on dart

--- super_camera/lib/src/camera.dart
+++ super_camera/lib/src/camera.dart
@@ -23,12 +23,50 @@ class Camera {
   }
 }
 
-class CameraException implements Exception {
-  const CameraException({this.code, this.description});
+class CameraExceptionType {
+  const CameraExceptionType._(this.value);
 
-  final String code;
+  static const CameraExceptionType cameraControllerNotOpen =
+      CameraExceptionType._('CameraControllerNotOpen');
+
+  static const CameraExceptionType delegateNameIsNull =
+      CameraExceptionType._('DelegateNameIsNull');
+
+  static const CameraExceptionType invalidSetting =
+      CameraExceptionType._('InvalidSetting');
+
+  static const CameraExceptionType miscellaneous =
+      CameraExceptionType._('Miscellaneous');
+
+  final String value;
+
+  List<CameraExceptionType> get values {
+    return <CameraExceptionType>[
+      cameraControllerNotOpen,
+      delegateNameIsNull,
+      invalidSetting,
+      miscellaneous,
+    ];
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  bool operator ==(other) {
+    return other is CameraExceptionType && this.value == other.value;
+  }
+
+  @override
+  String toString() => '$runtimeType($value)';
+}
+
+class CameraException implements Exception {
+  const CameraException({this.type, this.description});
+
+  final CameraExceptionType type;
   final String description;
 
   @override
-  String toString() => '$runtimeType($code, $description)';
+  String toString() => '$runtimeType($type, $description)';
 }
